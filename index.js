09cbfa914facf7d2cfb7224a3970b64400c8d97a
Suraj Patel <surajkumarpatel38@gmail.com>
2023-08-24 11:37:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+    return temperature;
+};
+// creating a server
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        requests("https://api.openweathermap.org/data/2.5/weather?q=Bilaspur&appid=1bd8e5987196d897110c4a1ebce27032")
+            .on("data", (chunk) => {
+                const objData = JSON.parse(chunk);
+                const arrData = [objData]
+                // console.log(arrData[0].main.temp);
+
+                //calling data to api and replace it
+                const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
+                res.write(realTimeData);
+                // console.log(realTimeData);
+            }).on("end", (err) => {
+                if (err) return console.log("connection cloced due to error", err);
+                res.end();
+            });
+    }
+});
+
+// starting server
+server.listen(8000, "127.0.0.1");
